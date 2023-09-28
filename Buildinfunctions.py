f105260e94e658734f3ba46dd0dea77be91b0d56
Sayeummahajan07 <100280315+Sayeum07@users.noreply.github.com>
2023-09-28 14:54:13 +0530

    Add files via upload

---
+++ Buildinfunctions.py
@@ -0,0 +1,22 @@
+a=-34
+print(abs(a))  #abs()
+
+
+b="Hello"
+print(len(b))  #len()
+
+
+c=[34,12,48,65]
+print(min(c))  #min()
+
+
+d=12.34
+print(round(d))#round()
+
+
+e='Hello12'
+print(e.isalnum())#isalnum()
+
+
+print(type(a))#type()
+
